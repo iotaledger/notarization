72202bf577e5765a2035790267ec60749aac183f
chrisgitiota <christof.gerritsma@iota.org>
2025-04-17 12:28:42 +0200

    Very raw but run-able Notarization e2e test frame and IOTA version pined to "v0.11.6-rc"
    For currently unknown reasons the test fails with `Error: No such file or directory (os error 2)`

--- notarization-rs/src/lib.rs
+++ notarization-rs/src/lib.rs
@@ -1,10 +1,12 @@
 // Copyright 2020-2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod client_tools;
 pub mod error;
 pub mod notarization;
 
-mod client_tools;
 pub mod core;
 mod iota_interaction_adapter;
 mod well_known_networks;
+
+pub use notarization::*;
