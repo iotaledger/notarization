13bb03f505a51fe40ba75d4fe3c737910f7fdf29
Christof Gerritsma <christof.gerritsma@iota.org>
2026-05-29 14:33:22 +0200

    feat: Update to iota v1.23.2 (#265)
    Summary:
    - [x] tokio version update to: 1.52.2
    - [ ] fastcrypto version update to rev = "-"
    - [x] notarization_wasm: new peerDep. version for @iota/iota-sdk: `"^1.14.0"`
    - [x] notarization_wasm: new dependency version for @iota/iota-interaction-ts: `"^0.14.0"`
    - [x] pin all product-core.git dependencies to `tag = "v0.8.18"`
    - [x] pin all iota.git dependencies to `tag = "v1.23.2"`
    
    IOTA 1.23.2 replaced several base-types formerly implemented in iota.git with types from the standalone Rust SDK (iota-rust-sdk.git). This caused several migrations in notarization_wasm and notarization_rs.

--- notarization-rs/src/core/move_utils.rs
+++ notarization-rs/src/core/move_utils.rs
@@ -4,10 +4,10 @@
 use std::str::FromStr;
 
 use iota_interaction::rpc_types::IotaObjectDataOptions;
-use iota_interaction::types::base_types::{ObjectID, ObjectRef};
+use iota_interaction::types::base_types::{ObjectID, ObjectRef, TypeTag};
 use iota_interaction::types::programmable_transaction_builder::ProgrammableTransactionBuilder as Ptb;
-use iota_interaction::types::transaction::{Argument, ObjectArg};
-use iota_interaction::types::{IOTA_CLOCK_OBJECT_ID, IOTA_CLOCK_OBJECT_SHARED_VERSION, TypeTag};
+use iota_interaction::types::transaction::{Argument, CallArg, SharedObjectRef};
+use iota_interaction::types::{IOTA_CLOCK_OBJECT_ID, IOTA_CLOCK_OBJECT_SHARED_VERSION};
 use iota_interaction::{IotaClientTrait, OptionalSync};
 use product_common::core_client::CoreClientReadOnly;
 use serde::Serialize;
@@ -16,11 +16,11 @@ use crate::error::Error;
 
 /// Adds a reference to the on-chain clock to `ptb`'s arguments.
 pub(crate) fn get_clock_ref(ptb: &mut Ptb) -> Argument {
-    ptb.obj(ObjectArg::SharedObject {
-        id: IOTA_CLOCK_OBJECT_ID,
+    ptb.obj(CallArg::Shared(SharedObjectRef {
+        object_id: IOTA_CLOCK_OBJECT_ID,
         initial_shared_version: IOTA_CLOCK_OBJECT_SHARED_VERSION,
         mutable: false,
-    })
+    }))
     .expect("network has a singleton clock instantiated")
 }
 
